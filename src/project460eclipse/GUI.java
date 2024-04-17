9590533ab88b6fae7e85598d9a3fb4bb9adbe41c
Joshua Greer <jjgreer@asu.edu>
2024-04-16 22:28:44 -0700

    I hate java
    and eclipse, and this project

---
+++ src/project460eclipse/GUI.java
@@ -0,0 +1,9 @@
+package project460eclipse;
+import javax.swing.*;
+import javax.swing.JFrame;
+
+public class GUI {
+	
+	JFrame gui = new JFrame();
+	
+}
---
+++ src/project460eclipse/Main.java
@@ -0,0 +1,16 @@
+package project460eclipse;
+import javax.swing.*;
+/*
+Joshua Greer 1218576515
+CSE460 Project/
+4/22/2024
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+	}
+
+}
